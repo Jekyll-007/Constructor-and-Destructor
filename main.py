88774ce59d4f09118734b4ae1a56d4dc9a3668e6
Jekyll-007 <teenofeejay@gmail.com>
2024-11-05 18:00:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Employees:
+    def __init__(self):
+        print("Employee Created!!")
+
+    def __def__(self):
+        print("Destructor called, employee deleted!!")
+obj=Employees()
+del obj
